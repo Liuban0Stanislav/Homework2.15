bf35458e107c062096e5941c292682c7f7d6f7fd
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-06-22 15:12:06 +1000

    ДЗ на проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        StringList stringList = new StringListImpl();
+        stringList.add("первый");
+        stringList.add("третий");
+        stringList.add("четвертый");
+        stringList.add("пятый");
+        System.out.println(stringList.toString());
+        System.out.println();
+
+        stringList.add(1, "второй");
+        System.out.println(stringList.toString());
+        System.out.println();
+
+        stringList.remove("пятый");
+        System.out.println(stringList.toString());
 
     }
 }
